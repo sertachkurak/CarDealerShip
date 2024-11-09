42973cd98dc5d6f096c30d22c8291f73f92a0cc1
SertacKurak <Sertackurak@students.softuni.bg>
2024-11-09 14:54:01 +0200

    Adding 2 Models

---
+++ CarDealerShip.Data.Models/VehicleCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShip.Data.Models
+{
+    using static Common.Constants.VehicleCategoryValidations;
+    public class VehicleCategory
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; }
+            = new List<Vehicle>();
+    }
+}
---
+++ CarDealerShip.Data.Models/VehicleType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShip.Data.Models
+{
+    using static Common.Constants.VehicleTypeValidations;
+
+    public class VehicleType
+    {
+
+        public Guid Id { get; set; }
+
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+
+    }
+}
