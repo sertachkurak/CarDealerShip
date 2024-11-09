a3ec3752829bda1458a78a6d5ba0ee11d9187395
SertacKurak <Sertackurak@students.softuni.bg>
2024-11-09 14:53:23 +0200

    Added Validations and DbContext

---
+++ CarDealerShip.Common/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShip.Common
+{
+
+    public static class Constants
+    {
+        public static class VehicleCategoryValidations
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 10;
+        }
+
+        public static class VehicleTypeValidations
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 10;
+        }
+    }
+}
---
+++ CarDealerShip.Data/DealerShipDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealerShip.Data
+{
+    public class DealerShipDbContext : IdentityDbContext
+    {
+
+        public DealerShipDbContext()
+        {
+            
+        }
+
+
+        public DealerShipDbContext(DbContextOptions options)
+                :base(options)
+        {
+            
+        }
+    }
+}
