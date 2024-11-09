b06076098773ac16f1ff26817ae1d411094a2f51
SertacKurak <Sertackurak@students.softuni.bg>
2024-11-09 21:33:29 +0200

    Add ApplicationUser

---
+++ CarDealerShip.Data.Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace CarDealerShip.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+
+        public ApplicationUser()
+        {
+            // ReSharper disable once VirtualMemberCallInConstructor
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
