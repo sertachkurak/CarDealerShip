838292b65b6678f6c1bd69e1529cf479bd6c0303
SertacKurak <Sertackurak@students.softuni.bg>
2024-11-09 21:47:05 +0200

    Implement DbContext

--- CarDealerShip.Data/DealerShipDbContext.cs
+++ CarDealerShip.Data/DealerShipDbContext.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using CarDealerShip.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarDealerShip.Data
 {
-    public class DealerShipDbContext : IdentityDbContext
+    public class DealerShipDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
 
         public DealerShipDbContext()
@@ -23,5 +25,16 @@ namespace CarDealerShip.Data
         {
             
         }
+
+        public virtual DbSet<Vehicle> Vehicles { get; set; } = null!;
+        public virtual DbSet<VehicleCategory> VehiclesCategories { get; set; } = null!;
+        public virtual DbSet<VehicleType> VehiclesTypes { get; set; } = null!;
+        public virtual DbSet<VehiclePurpose> VehiclesPurposes { get; set; } = null!;
+        public virtual DbSet<Location> Locations { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        } 
     }
 }
